ba362fa983e7e2aa0a735aef776b624b756cd221
Mikhail Tolstenev <Tolstenev96@gmail.com>
2024-06-27 21:01:21 +0300

    commit1

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,8 +6,8 @@ role varchar (255) not null
  );
  insert into Users (login, password, role)
  values ('user1', '$2y$10$hIG4yTPcg8lltDVFTLPXre9QOOMsDgckx6GH71H7wVHhOIvzkjg12', 'ROLE_USER');
- insert into Users (login, password, role)
- values ('user2', '$2y$10$rXoT/uuSXT/iJ1/hHxhkfe5NcaHzIkaPMVTKtSUIehq3S1XZSbtJm', 'ROLE_USER');
+-- insert into Users (login, password, role)
+-- values ('user2', '$2y$10$rXoT/uuSXT/iJ1/hHxhkfe5NcaHzIkaPMVTKtSUIehq3S1XZSbtJm', 'ROLE_USER');
 
 -- create table if not exists Files(
 -- id serial primary key,
